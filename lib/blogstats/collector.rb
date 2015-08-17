17d7d0b4815cfffcb4af6b7e06d8a32b8cf87464
Randy Coulman <rcoulman@gmail.com>
2015-08-16 18:15:20 -0700

    Clean up Collector implementation
    * Extract constants for various tag regexes
    * Extract some methods

--- lib/blogstats/collector.rb
+++ lib/blogstats/collector.rb
@@ -18,35 +18,43 @@ module Blogstats
 
     attr_reader :input
 
-    JEKYLL_TAG = /^\{% .* %\}$/
+    BEGIN_CODE_BLOCK = /^\{%\s+codeblock.*%\}$/
+    END_CODE_BLOCK = /^\{%\s+endcodeblock.*%\}$/
+    VIDEO_TAG = /^\{%\s+youtube.*%\}$/
+    IMAGE_TAG = /^\{%\s+img.*%\}$/
+    OTHER_JEKYLL_TAG = /^\{% .* %\}$/
 
     def collect_stats
+      skip_yaml_front_matter
+      process_post_body
+    rescue StopIteration
+    end
+
+    def process_post_body
       stats.add_post
 
-      skip_yaml_front_matter
       while line = input.next.strip
-        if line =~ /^\{%\s+codeblock.*%\}$/ .. line =~ /^\{%\s+endcodeblock.*%\}$/
-          process_loc(line)
+        if line =~ BEGIN_CODE_BLOCK .. line =~ END_CODE_BLOCK
+          process_code_block(line)
         else
           process_line(line)
         end
       end
-    rescue StopIteration
     end
 
-    def process_loc(line)
-      return if line =~ JEKYLL_TAG
-
-      stats.add_loc
+    def process_code_block(line)
+      unless line =~ BEGIN_CODE_BLOCK || line =~ END_CODE_BLOCK
+        stats.add_loc
+      end
     end
 
     def process_line(line)
       case line
-        when /^\{%\s+youtube.*%\}$/
+        when VIDEO_TAG
           stats.add_video
-        when /^\{%\s+img.*%\}$/
+        when IMAGE_TAG
           stats.add_image
-        when JEKYLL_TAG
+        when OTHER_JEKYLL_TAG
           return
         else
           stats.add_words(line.split.count)
